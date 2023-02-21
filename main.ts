58d265f246a8d5ed3acb328601f34792a9f57288
AKB0911 <125360581+AKB0911@users.noreply.github.com>
2023-02-21 10:40:23 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,38 @@
+let LFSL = 0
+let LFSR = 0
+function Soft_left () {
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 60)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
+}
+function Hard_left () {
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 60)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 60)
+    basic.pause(100)
+}
+function Hard_right () {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 60)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 60)
+    basic.pause(100)
+}
+function Stright () {
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 60)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 60)
+}
 function soft_right () {
-	
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 60)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
 }
 basic.forever(function () {
-	
+    LFSL = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
+    LFSR = maqueen.readPatrol(maqueen.Patrol.PatrolRight)
+    if (LFSL == 1 && LFSR == 1) {
+        Stright()
+    }
+    if (LFSL == 1) {
+        soft_right()
+    } else if (LFSR == 1) {
+        Soft_left()
+    } else {
+        Stright()
+    }
 })
